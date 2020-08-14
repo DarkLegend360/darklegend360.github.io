fd4253b22c9de70039849d7d4d65a6960336e613
DarkLegend360 <darklegend360@live.com>
2020-08-14 23:42:01 +0530

    Updates

---
+++ precache-manifest.d8ca7bf6bed18e69214337028657e563.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20ad65f10e618d1776d82e930b7d646",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40ca7675fb37b5ad6e18",
+    "url": "/static/css/main.c93fbbc3.chunk.css"
+  },
+  {
+    "revision": "950c89d62c1dcff16b28",
+    "url": "/static/js/2.ce234028.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ce234028.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40ca7675fb37b5ad6e18",
+    "url": "/static/js/main.4dc1ac0a.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "cae4bb4d393245370f69423747801424",
+    "url": "/static/media/wall.cae4bb4d.jpg"
+  }
+]);
